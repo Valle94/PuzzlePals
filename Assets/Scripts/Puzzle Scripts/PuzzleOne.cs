5fd3169bcbbf4ebbe084617a35681ea00f0cb7e9
GabbiePurch <purchi375@morrisville.edu>
2026-09-15 12:52:13 -0400

    Started Puzzle One Script
    Started to set ground work for puzzle one script.

--- Assets/Scripts/Puzzle Scripts/PuzzleOne.cs
+++ Assets/Scripts/Puzzle Scripts/PuzzleOne.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 
 public class PuzzleOne : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshPro ansText;
+    [SerializeField] private TMP_Text ranShapetext;
+    [SerializeField] private Canvas playerMKCanvas;
+    [SerializeField] private Canvas playerCCanvas;
+    [SerializeField] private CharacterController playerMK;
+    [SerializeField] private CharacterController playerC;
+
+    [SerializeField] private List<string> passShapeList = new List<string>() {"Cube", "Diamond", "Triangle", "Square"};
     void Start()
+    {
+        string shapeAns = GetRandomShape(passShapeList);
+        ranShapetext.text = shapeAns;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+// The Function Gets a random index from the list given and returns that string to be used as our password shape
+    public string GetRandomShape(List<string> passwordList)
     {
-        
+        int randomIndex = Random.Range(0, passwordList.Count);
+
+        return passwordList[randomIndex];
     }
 }
